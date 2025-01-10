368573faa5745515640933df9329d50e5507bbec
Livreur <coulontitouann@gmail.com>
2025-01-10 22:21:27 +0100

    part2 1/2

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ Une image arbitraire peut avoir des pixels de nature différente:
 Passer l’image d’entrée en mode rgb8, c’est-à-dire avec 3 canaux R, G, B, représentés chacun par un u8.
 ```
 
-Pour ouvrir une image depuis un fichier, on utilise la fonction open() du module image::io::Reader.
+Pour ouvrir une image depuis un fichier, on utilise la fonction open() du crate image.
 ```rust
 use image::GenericImageView;
 
@@ -71,7 +71,7 @@ Pixel (32, 52) : Rgba([52, 101, 19, 255])
 ```
 Passer un pixel sur deux d’une image en blanc. Est-ce que l’image obtenue est reconnaissable?
 ```
-![image chargee uniquement apres avoir build & run le projet](images/output/halfed_image.png)
+![image chargee uniquement apres avoir build & run le projet | image avec 1 pixel sur 2 en blanc](images/output/halfed_image.png)
 ```rust
 let mut halfed_img = img.clone();
 for y in 0..halfed_img.height() {
@@ -83,3 +83,68 @@ for y in 0..halfed_img.height() {
 }
 halfed_img.save("images/output/halfed_image.png");
 ```
+
+## Partie 2:
+
+### Question 6:
+```
+Comment récupérer la luminosité d’un pixel?
+```
+
+Selon [wikipédia](https://fr.wikipedia.org/wiki/Luminance#Matri%C3%A7age), le luminance relative est calculée par la formule suivante:
+```
+Y = 0.2126 * R + 0.7152 * G + 0.0722 * B
+```
+Je vais donc utiliser cette formule pour calculer la luminosité d'un pixel.
+
+### Question 7:
+```
+Implémenter le traitement
+```
+```rust
+let img = image.to_rgb8(); // L'image de l'iut est un jpg donc on la convertit en rgb8
+let mut new_img = img.clone();
+for y in 0..img.height() {
+    for x in 0..img.width() {
+        let pixel = img.get_pixel(x, y);
+        let Rgb([r, g, b]) = pixel;
+        let luminance = 0.2126 * (*r as f32) + 0.7152 * (*g as f32) + 0.0722 * (*b as f32);
+        new_img.put_pixel(x, y, image::Rgb([if luminance > 128.0 { 255 } else { 0 }; 3]));
+    }
+}
+
+halfed_img.save("images/output/iut_black_and_white.png");
+```
+![iut.jpg](images/iut.jpg) -> ![image chargee uniquement apres avoir build & run le projet | iut_black_and_white.png](images/output/iut_black_and_white.png)
+
+### Question 8:
+```
+Permettre à l’utilisateurice de remplacer "noir" et "blanc" par une paire de couleurs au choix.
+```
+On passe le code de la question 7 dans une fonction qui prend en paramètre les couleurs à utiliser et on appel cette fonction avec les couleurs noires et blanches.
+```rust
+fn black_and_white_image(image: &image::DynamicImage) -> image::DynamicImage {
+    return change_color_palette(&image.clone(), image::Rgb([255; 3]), image::Rgb([0; 3]));
+}
+
+fn change_color_palette(image: &image::DynamicImage, color1:image::Rgb<u8>, color2:image::Rgb<u8>) -> image::DynamicImage {
+    let img = image.to_rgb8();
+    let mut new_img = img.clone();
+
+    for y in 0..img.height() {
+        for x in 0..img.width() {
+            let pixel = img.get_pixel(x, y);
+            let Rgb([r, g, b]) = pixel;
+            let luminance = 0.2126 * (*r as f32) + 0.7152 * (*g as f32) + 0.0722 * (*b as f32);
+            new_img.put_pixel(x, y, if luminance > 128.0 { color1 } else { color2 });
+        }
+    }
+
+    image::DynamicImage::ImageRgb8(new_img)
+}
+```
+```rust
+let red_and_blue = change_color_palette(&image_iut, image::Rgb([255, 0, 0]), image::Rgb([0, 0, 255]));
+red_and_blue.save("images/output/iut_red_and_blue.png").expect("Failed to save image");
+```
+![image chargee uniquement apres avoir build & run le projet | palette de couleurs rouge et bleu](images/output/iut_red_and_blue.png)
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,93 @@
-use image::{GenericImageView, Rgba};
+use image::{DynamicImage, GenericImage, GenericImageView, Rgba, Rgb};
+
+trait DynamicImageExtensions {
+    fn set_half_pixels_white(&mut self) -> ();
+    fn set_black_and_white(&mut self) -> ();
+    fn set_monochrome_color_palette(&self, color1: Rgb<u8>, color2: Rgb<u8>) -> ();
+}
+
+impl DynamicImageExtensions for DynamicImage {
+    fn set_half_pixels_white(&mut self) -> () {
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                if (x + y) % 2 == 0 {
+                    self.put_pixel(x, y, image::Rgba([255, 255, 255, 1]));
+                }
+            }
+        }
+    }
+
+    fn set_monochrome_color_palette(&self, color1: [u8;3], color2: [u8;3]) -> () {
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                let pixel = self.get_pixel(x, y);
+                let Rgba([r, g, b, a]) = pixel;
+                let luminance = 0.2126 * (*r as f32) + 0.7152 * (*g as f32) + 0.0722 * (*b as f32);
+                self.put_pixel(x, y, if luminance > 128.0 { Rgba(color1 } else { color2 });
+            }
+        }
+    }
+
+    fn set_black_and_white(&mut self) -> () {
+        self.set_monochrome_color_palette(image::Rgb([255; 3]), image::Rgb([0; 3]));
+    }
+}
+
+fn black_and_white_image(image: &image::DynamicImage) -> image::DynamicImage {
+    return change_color_palette(&image.clone(), image::Rgb([255; 3]), image::Rgb([0; 3]));
+}
+
+fn change_color_palette(image: &image::DynamicImage, color1:image::Rgb<u8>, color2:image::Rgb<u8>) -> image::DynamicImage {
+    let img = image.to_rgb8();
+    let mut new_img = img.clone();
+
+    for y in 0..img.height() {
+        for x in 0..img.width() {
+            let pixel = img.get_pixel(x, y);
+            let Rgb([r, g, b]) = pixel;
+            let luminance = 0.2126 * (*r as f32) + 0.7152 * (*g as f32) + 0.0722 * (*b as f32);
+            new_img.put_pixel(x, y, if luminance > 128.0 { color1 } else { color2 });
+        }
+    }
+
+    image::DynamicImage::ImageRgb8(new_img)
+}
+
+fn save_image_png(image: image::DynamicImage, image_name: &str) {
+    save_image(&image, &image_name, "png");
+}
+
+fn save_image(image: &image::DynamicImage, image_name: &str, extension: &str) {
+    let folder_path = "images/output";
+    let file_path = format!("{}/{}.{}", folder_path, image_name, extension);
+
+    image.save(file_path.clone()).expect(&format!("Failed to save image to {}", file_path));
+}
 
 fn main() {
     let img = image::open("images/example.jpg").expect("Failed to open image");
     println!("Dimensions: {:?}", img.dimensions());
     println!("Color type: {:?}", img.color());
-    let rgb_image = img.to_rgb8();
-    rgb_image.save("images/output/rgb_image.png").expect("Failed to save image");
 
-    let logo = image::open("images/logo.png").expect("Failed to open image");
-    let rgb_logo = logo.to_rgb8();
-    rgb_logo.save("images/output/rgb_logo.png").expect("Failed to save image");
+    let rgb_image = img.clone().to_rgb8();
+    save_image_png(image::DynamicImage::ImageRgb8(rgb_image), "rgb_image");
 
-    get_pixel_color(&img, 32, 52);
+    let mut pngalpha = image::open("images/pngalpha.png").expect("Failed to open image"); // Image avec un canal alpha de base
+    pngalpha = image::DynamicImage::ImageRgb8(pngalpha.to_rgb8());
+    save_image_png(pngalpha.clone(), "pngalpha");
 
-    let halfed_image = half_pixels_white(&img);
-    halfed_image.save("images/output/halfed_image.png").expect("Failed to save image");
-}
+    let Rgba([r, g, b, a]) = img.get_pixel(32, 52);
+    println!("Pixel color - Red: {}, Green: {}, Blue: {}, Alpha: {}", r, g, b, a);
 
-fn get_pixel_color(image: &image::DynamicImage, x: u32, y: u32) -> Rgba<u8> {
-    let pixel = image.get_pixel(x, y);
-    match pixel {
-        Rgba([r, g, b, a]) => {
-            println!("Pixel color at ({}, {}): R: {}, G: {}, B: {}, A: {}", x, y, r, g, b, a);
-            Rgba([r, g, b, a])
-        }
-    }
-}
+    let mut half_white = img.clone();
+    half_white.set_half_pixels_white();
+    save_image_png(half_white, "half_white");
 
-fn half_pixels_white(image: &image::DynamicImage) -> image::DynamicImage {
-    let mut img = image.to_rgb8();
+    let image_iut = image::open("images/iut.jpg").expect("Failed to open image");
+    
+    let iut_black_and_white = black_and_white_image(&image_iut);
+    save_image_png(iut_black_and_white, "iut_black_and_white");
 
-    for y in 0..img.height() {
-        for x in 0..img.width() {
-            if (x + y) % 2 == 0 {
-                img.put_pixel(x, y, image::Rgb([255, 255, 255]));
-            }
-        }
-    }
-
-    image::DynamicImage::ImageRgb8(img)
+    let iut_red_and_blue = change_color_palette(&image_iut, image::Rgb([255, 0, 0]), image::Rgb([0, 0, 255]));
+    save_image_png(iut_red_and_blue, "iut_red_and_blue");
 }
